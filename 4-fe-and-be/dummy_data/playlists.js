fd98e214cdda0e789f88e717ccf2bd20f3ea261f
ujikstark <ujikbauk@gmail.com>
2023-07-14 11:26:43 +0700

    add sample playlist dummy data

---
+++ 4-fe-and-be/dummy_data/playlists.js
@@ -0,0 +1,10 @@
+exports.playlist = [
+    { id: 1, name: 'Lagu-lagu santai', songs: [
+        { id: 1, title: 'Monokrom', artist: 'Tulus', music: "music/monokrom-tulus.mp3", img: "img/tulus-monokrom.jpeg" },
+        { id: 2, title: 'Sial', artist: 'Mahalini', music: "music/sial-mahalini.mp3", img: "img/sial-mahalini.jpeg" },
+    ]},
+    { id: 1, name: 'Lagu-lagu islami', songs: [
+        { id: 1, title: 'Sial', artist: 'Mahalini', music: "music/sial-mahalini.mp3", img: "img/sial-mahalini.jpeg" },
+        { id: 2, title: 'Monokrom', artist: 'Tulus', music: "music/monokrom-tulus.mp3", img: "img/tulus-monokrom.jpeg" },
+    ]},
+]
