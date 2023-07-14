7979cd507baac88b3c8c907b3132101742d75d56
ujikstark <ujikbauk@gmail.com>
2023-07-14 11:08:01 +0700

    modified all songs api

--- 4-fe-and-be/backend.js
+++ 4-fe-and-be/backend.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const songs = require('./dummy_data/songs');
 
 const app = express();
 const port = 3000;
@@ -7,12 +8,6 @@ const port = 3000;
 app.use(cors());
 
 app.get('/songs', (req, res) => {
-
-    var songs = [
-        {id: 1, title: 'Songs 1', artist: 'Artist 1', played_count: 2},
-        {id: 2, title: 'Songs 2', artist: 'Artist 2', played_count: 5},
-        {id: 3, title: 'Songs 3', artist: 'Artist 3', played_count: 10},
-    ];
 
     res.status(200).send({data: songs});
 })
--- 4-fe-and-be/dummy_data/songs.js
+++ 4-fe-and-be/dummy_data/songs.js
@@ -1,6 +1,4 @@
-export default {
-    songs: [
-        {id: 1, title: 'Monokrom', artist: 'Tulus', music: "music/monokrom-tulus.mp3", img: "img/tulus-monokrom.jpeg"},
-        {id: 2, title: 'Sial', artist: 'Mahalini', music: "music/sial-mahalini.mp3", img: "img/sial-mahalini.jpeg"},
-    ]
-}
+exports.songs = [
+    { id: 1, title: 'Monokrom', artist: 'Tulus', music: "music/monokrom-tulus.mp3", img: "img/tulus-monokrom.jpeg" },
+    { id: 2, title: 'Sial', artist: 'Mahalini', music: "music/sial-mahalini.mp3", img: "img/sial-mahalini.jpeg" },
+]
