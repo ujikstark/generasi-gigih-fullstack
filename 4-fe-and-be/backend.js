d9691575e078f0228fc209c09660b3dccdafcbff
ujikstark <ujikbauk@gmail.com>
2023-07-15 10:41:10 +0700

    add post playlists api

--- 4-fe-and-be/backend.js
+++ 4-fe-and-be/backend.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+const { v4: uuid } = require("uuid");
+
+
 const songs = require('./dummy_data/songs');
 
 const app = express();
@@ -11,6 +14,18 @@ app.get('/songs', (req, res) => {
 
     res.status(200).send({data: songs});
 })
+
+app.post('/playlists', express.json(), async (req, res) => {
+    const { name } = req.body;
+
+    const newPlaylist = {
+        id: uuid(),
+        name: name,
+    }
+
+    res.status(201).send({message: "Created new playlist!", data: newPlaylist});
+});
+
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
